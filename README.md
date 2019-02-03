732dc1338df7ccfe00d1b2cd214cac5e82146339
Subhaditya Mukherjee <26865436+SubhadityaMukherjee@users.noreply.github.com>
2019-02-03 18:39:18 +0530

    Added files

---
+++ README.md
@@ -0,0 +1,15 @@
+# README
+
+This application lets you draw simple math equations in the air and solves them .
+You can also use it to just draw something and save it.
+
+The recogniton part is actually from another module Ive made before. PicCalc. 
+https://github.com/SubhadityaMukherjee/Pic_Calc
+
+## Note
+Choose a color you will use to draw by running the command 
+python range_det.py -f HSV -w
+
+After that run python draw.py
+
+
---
+++ draw.py
@@ -0,0 +1,69 @@
+import cv2
+import numpy as np
+import pickle
+import training as tr
+cap = cv2.VideoCapture(0)
+
+points = []
+
+flag = 0
+
+while(1):
+
+  _,frame = cap.read()
+  frame = cv2.flip(frame,1)
+
+  filterFrame = cv2.GaussianBlur(frame,(35,35),25)
+
+  hsvFrame = cv2.cvtColor(filterFrame,cv2.COLOR_BGR2HSV)
+  with open('range.pickle','rb') as f:
+    t = pickle.load(f)
+  lower_bound = np.array([t[0],t[1],t[2]])
+  upper_bound = np.array([t[3],t[4],t[5]])
+
+
+  threshImg = cv2.inRange(hsvFrame,lower_bound,upper_bound) 
+
+  _,contours,_ = cv2.findContours(threshImg,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+  finalImg = cv2.bitwise_and(frame,frame,mask=threshImg)
+
+  finalImg = cv2.drawContours(finalImg,contours,-1,(255,255,0),1)
+
+  c,X,Y=0,0,0
+
+  key = cv2.waitKey(1)
+
+  if flag==1:
+    for item in contours:
+      for i in item :
+        X += i[0][0]
+        Y += i[0][1]
+        c += 1
+
+    try:      
+      points.append([int(X/c),int(Y/c)])    
+    except:
+      pass
+
+  if (key & 0xFF == ord('s')) and flag == 0:
+    flag = 1
+
+  elif key & 0xFF == ord('s') and flag == 1 :
+    flag = 0
+  for p in points:
+      cv2.circle(finalImg,tuple(p),20,(250,250,250),-1) 
+
+  cv2.imshow('Draw',finalImg)
+
+  if key & 0xFF == ord('q'):
+    cv2.imwrite('new.jpg', cv2.bitwise_not(finalImg))
+    cap.release()
+    cv2.destroyAllWindows()
+    break
+
+cv2.destroyAllWindows()
+#cap.release()
+
+tr.test()
+
